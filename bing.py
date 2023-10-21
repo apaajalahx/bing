57e3ee9e221519a653fffc668448f334e67b6f06
Dinar Hamid <52324987+apaajalahx@users.noreply.github.com>
2023-10-21 14:39:16 +0700

    Update bing.py

--- bing.py
+++ bing.py
@@ -1,30 +1,26 @@
 #!/bin/python3
-# code by Dinar fb.com/dinar1337
-import requests
+from selenium import webdriver
 import re
 
-def search(dork):
-    try:
-        headers = {'User-Agent':'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0'}
-        maxs = 300
-        for x in range(1,maxs):
-            np = x * 10
-            req = requests.get("http://www.bing.com/search?q=" + dork + "&first=" + str(np), headers=headers)
-            result = re.findall('<h2><a target="_blank" href="(.*?)"',str(req.content))
-            for i in result:
-                o = i.split('/')
-                print(o[2])
-                save(o[2])
-    except Exception as e:
-        print(e)
-    except ConnectionError as e:
-        print(e)
+def save(text):
+    z = open('result-bing.txt', 'a+')
+    z.write(text + "\n")
+    z.close()
 
-def save(ss):
-    s = open('result.txt', 'a+')
-    for u in ss:
-        s.write(u + "\n")
-    s.close()
+def reg(source, page):
+    search = re.findall("<cite>(.*?)</cite>", source)
+    if len(search) > 0:
+        print("GET {} DOMAIN FROM PAGE {}".format(len(search), str(page)))
+        save("\n".join(search))
 
-s = input('INPUT ANJING: ')
-search(s)
+def main(dork):
+    for x in range(0,300):
+        np = (x * 10) + 1
+        driver = webdriver.Chrome()
+        driver.get('https://www.bing.com/search?q='+ dork +'&first='+str(np)+'&rdr=1&rdrig=7784A68910F94CDE9DFA1DBF740D3A29')
+        reg(driver.page_source, np)
+        driver.close()
+    driver.quit()
+
+z = input("DORK : ")
+main(z)
